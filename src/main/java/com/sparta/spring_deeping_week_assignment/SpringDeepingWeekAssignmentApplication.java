5a6828124086b1bd8e5ac02db8a1cfa134779520
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-01-09 13:39:32 +0900

    Initial commit

---
+++ src/main/java/com/sparta/spring_deeping_week_assignment/SpringDeepingWeekAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_deeping_week_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeepingWeekAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDeepingWeekAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/spring_deeping_week_assignment/SpringDeepingWeekAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_deeping_week_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeepingWeekAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
